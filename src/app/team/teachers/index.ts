87dd1411acc810a23e3a0832446d1c7352e983f0
Steve Ayers <steveneayers@gmail.com>
2019-08-19 20:56:12 -0500

    Add ashley

--- src/app/team/teachers/index.ts
+++ src/app/team/teachers/index.ts
@@ -1,5 +1,6 @@
 import { Teacher } from './teacher.model';
 import { Nik } from './nik.model';
 import { Jacquie } from './jacquie.model';
+import { Ashley } from './ashley.model';
 
-export const Teachers: Teacher[] = [new Nik(), new Jacquie()];
+export const Teachers: Teacher[] = [new Nik(), new Jacquie(), new Ashley()];
