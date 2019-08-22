f204ff1a2341f17d6040e804302dd38f18831436
Steve Ayers <steveneayers@gmail.com>
2019-08-22 17:42:57 -0500

    add Dana and Jenine

---
+++ src/app/team/teachers/dana.model.ts
@@ -0,0 +1,9 @@
+import { Teacher } from './teacher.model';
+
+declare var require: any;
+export class Dana extends Teacher {
+  name: string = 'Dana';
+  image: any = require('./dana.jpg');
+  webp: any = require('./dana.webp');
+  description: string = `<p>Dana danced from the age of 3 through college. Dance and education have always been a special part of her heart. Dana attended Northern Illinois University to obtain a degree in Special Education and a minor in Dance Education. She has additional education in Behavioral Analysis and Autism. Combining her passion to help students with disabilities have as many opportunities as possible and her love for dance has always been something she's dreamed about. Dana is so thankful for the opportunity to do so!</p>`;
+}
--- src/app/team/teachers/index.ts
+++ src/app/team/teachers/index.ts
@@ -3,10 +3,14 @@ import { Nik } from './nik.model';
 import { Jacquie } from './jacquie.model';
 import { Ashley } from './ashley.model';
 import { Tammy } from './tammy.model';
+import { Jenine } from './jenine.model';
+import { Dana } from './dana.model';
 
 export const Teachers: Teacher[] = [
   new Nik(),
   new Jacquie(),
   new Ashley(),
-  new Tammy()
+  new Tammy(),
+  new Jenine(),
+  new Dana()
 ];
